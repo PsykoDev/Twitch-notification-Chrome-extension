6f60f9ee9daa7bd5bcb423841abbc93b6157b587
PsykoDev <45910905+PsykoDev@users.noreply.github.com>
2020-04-30 15:27:11 +0200

    Add files via upload

---
+++ js/chrome.js
@@ -0,0 +1,32 @@
+var Biche_nav = {
+    doNotification: function() {
+        chrome.notifications.clear('notifyON' + Biche_params.title, function(id) { });
+        chrome.notifications.create('notifyON' + Biche_params.title, { type: "basic", title: Biche_params.title, message: Biche_params.message, iconUrl: "img/iconon128.png" }, function(id) { });
+    },
+    setIconON: function(on) {
+        var status = on ? "on" : "off";
+        chrome.browserAction.setIcon({path : "img/icon" + status + "48.png"});
+    },
+    goIt: function() {
+        if (Biche.isON){
+            chrome.tabs.create({ url: Biche.getCurrentRedirectUrl()},function(tab){});
+        } else {
+            chrome.tabs.create({ url:Biche_params.offlineUrl},function(tab){});
+        }
+    }
+}
+
+chrome.browserAction.onClicked.addListener(Biche_nav.goIt);
+chrome.notifications.onClicked.addListener(function(notificationId){
+    if (notificationId === 'notifyON' + Biche_params.title) {
+        chrome.tabs.create({ url: Biche.getCurrentRedirectUrl()},function(tab){});
+    }
+});
+
+Biche_nav.setIconON(false);
+var Biche = new BtnLive(Biche_params.chaines, function(result) {
+    Biche_nav.setIconON(result);
+    if (result) {
+        Biche_nav.doNotification();
+    }
+}, 60000, 2);
---
+++ js/parameters.js
@@ -0,0 +1,1 @@
+var Biche_params = { "title": "Thewinlo31", "message": "Il est en ligne !", "offlineUrl": "http:\/\/www.twitch.tv\/Thewinlo31", "chaines": [{ "id": "Thewinlo31", "type": "twitch", "redirect": "http:\/\/www.twitch.tv\/Thewinlo31", "key": "" }] }; if (typeof require !== "undefined") { exports.Biche_params = Biche_params;}
